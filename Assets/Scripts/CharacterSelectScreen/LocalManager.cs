6f55f21b8592e56bf29a2e04e3f0e4a9ae4a26f2
Tim S <MangaSketch@gmail.com>
2015-11-22 12:04:22 -0500

    Started a character select scene

---
+++ Assets/Scripts/CharacterSelectScreen/LocalManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class LocalManager : MonoBehaviour
+{
+    [SerializeField] GameObject destinationSpot;
+
+    private void Awake()
+    {
+        Pooler();
+    }
+}
---
+++ Assets/Scripts/CharacterSelectScreen/Pooler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public partial class LocalManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> playerCharacter = new List<GameObject>();
+    [SerializeField] GameObject poolerLocation;
+
+    private void Pooler()
+    {
+        if(playerCharacter.Count > 0)
+        {
+            foreach(GameObject PC in playerCharacter)
+            {
+                Instantiate(PC, poolerLocation.transform.position, poolerLocation.transform.rotation);
+                PC.SetActive(false);
+            }
+        }
+    }
+}
