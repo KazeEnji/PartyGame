9ebbb29fd12fbbc78e1885bf440f6d2fba2387bc
Tim S <MangaSketch@gmail.com>
2015-11-28 12:02:23 -0500

    Commented the scripts.

--- Assets/Scripts/CharacterSelectScreen/LocalManager.cs
+++ Assets/Scripts/CharacterSelectScreen/LocalManager.cs
@@ -3,42 +3,53 @@ using System.Collections;
 
 public partial class LocalManager : MonoBehaviour
 {
-    
+    //These variables are for holding onto the posisions to make the character selects work.
     [SerializeField] private GameObject destinationSpot;
     [SerializeField] private GameObject activeModel;
     [SerializeField] private int pointInList = 0;
     [SerializeField] private int indexesForPlayerCharacters;
 
+    //Calls the pooler for grabbing the models.
     private void Awake()
     {
         Debug.Log("Loading Start");
         Pooler();
+
+        //Gets the indexes of the instantiated list for characeter switching later.
         indexesForPlayerCharacters = playerCharacters.Count - 1;
         Debug.Log("Loading Done");
+
+        //Displays the character models.
         ShowPCChoices();
     }
 
     private void Update()
     {
+        //Waits for keypress to switch characters on screen.
         NextPC();
         PreviousPC();
     }
     
     private void ShowPCChoices()
     {
+        //Checks to see if there's already a model loaded
         if(activeModel != null)
         {
+            //If there is, disable it and move it back to the pooler location
             activeModel.SetActive(false);
             activeModel.transform.position = poolerLocation.transform.position;
             activeModel.transform.rotation = poolerLocation.transform.rotation;
         }
 
+        //If there isn't a model loaded, grab one, move it into position, and turn it on.
         activeModel = playerCharacters[pointInList];
         activeModel.transform.position = destinationSpot.transform.position;
         activeModel.transform.rotation = destinationSpot.transform.rotation;
         activeModel.SetActive(true);
     }
 
+    //Advance to the next model in the list on keypress.
+    //This will eventually be changed for joystick input.
     private void NextPC()
     {
         if(Input.GetKeyDown(KeyCode.C))
@@ -55,6 +66,8 @@ public partial class LocalManager : MonoBehaviour
         }
     }
 
+    //Advance to the previous model in the list on keypress.
+    //This will eventually be changed for joystick input.
     private void PreviousPC()
     {
         if (Input.GetKeyDown(KeyCode.Z))
--- Assets/Scripts/CharacterSelectScreen/Pooler.cs
+++ Assets/Scripts/CharacterSelectScreen/Pooler.cs
@@ -4,22 +4,36 @@ using System.Collections.Generic;
 
 public partial class LocalManager : MonoBehaviour
 {
+    //List for prefabs.
     [SerializeField] private List<GameObject> poolerList = new List<GameObject>();
+
+    //List for instantiated objects.
     [SerializeField] private List<GameObject> playerCharacters = new List<GameObject>();
+
+    //Holds onto the pooler's location.
     [SerializeField] private GameObject poolerLocation;
 
     private void Pooler()
     {
+        //Checks to make sure there are objects in the list.
         if(poolerList.Count > 0)
         {
+            //Loops through the prefab list.
             foreach(GameObject _pc in poolerList)
             {
                 GameObject _tempPC;
+
+                //Instantiates the prefab and assigns it to a variable.
                 _tempPC = (GameObject)Instantiate(_pc, poolerLocation.transform.position, poolerLocation.transform.rotation);
+                
+                //Activates the instantiated object.
                 _tempPC.SetActive(false);
+                
+                //Adds the instantiated object to a different list.
                 playerCharacters.Add(_tempPC);
             }
         }
+        //If not, debug the error.
         else
         {
             Debug.Log("There are no objects in List.");
